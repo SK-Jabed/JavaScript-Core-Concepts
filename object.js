33568d1794d8f69a0ab162b0d4f944c723598867
Sheikh Jabed <jabedbd2295@gmail.com>
2024-09-13 23:16:25 +0600

    Object JS File Done and Completed

--- object.js
+++ object.js
@@ -3,13 +3,27 @@ const student = {
     roll: 658840,
     address: "Dhaka",
     isSingle: true,
-    friends: ["Apu", "Dipu", "Tipu", "Shipu"],
+    friends: ["Ratul", "Shihab", "Rafi", "Fahad"],
+    grades: [{class: "Five", year: 2015, grade: 5.00}, {class: "Ten", year: 2021, grade: 5.00}],
+    department: function() {
+        console.log("Computer Science and Technology");
+    },
     car: {
-        brand: "Ferrari",
+        brand: "Tesla",
         Price: 50000000,
         made: 2020,
-        color: "Red"
+        color: "Black",
+        manufacturer: {
+            name: "Tesla",
+            ceo: "Elon Mask",
+            country: "USA"
+        }
     }
 }
 
 console.log(student);
+console.log(student.name);
+console.log(student.friends);
+console.log(student.car);
+console.log(student.grades);
+console.log(student.car.manufacturer);
