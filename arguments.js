351338645ff1fbec52d40e3dc4a56e5c773fd691
Sheikh Jabed <jabedbd2295@gmail.com>
2024-09-13 23:43:14 +0600

    Arguments JS and Array JS File Done

---
+++ arguments.js
@@ -0,0 +1,6 @@
+function add(num1, num2) {
+    console.log(num1, num2);
+    console.log(arguments[4]);
+}
+
+add(11, 22, 33, 44, 55);
---
+++ array.js
@@ -0,0 +1,14 @@
+// const products = [
+//     {name: "Phone", price: 27000},
+//     {name: "Laptop", price: 70000},
+// ]
+
+// products[0];
+// products[1];
+
+// const products = [15, 56, 87]
+const products = { 
+    "0": 15,
+    "1": 56,
+    "2": 87
+}
