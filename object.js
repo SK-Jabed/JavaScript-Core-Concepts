41c98f4852cf66555453c4df6f7478dbcf59efb0
Sheikh Jabed <jabedbd2295@gmail.com>
2024-09-13 22:56:11 +0600

    JavaScript Core Concepts Repository Added

---
+++ object.js
@@ -0,0 +1,15 @@
+const student = {
+    name: "Sheikh Jabed",
+    roll: 658840,
+    address: "Dhaka",
+    isSingle: true,
+    friends: ["Apu", "Dipu", "Tipu", "Shipu"],
+    car: {
+        brand: "Ferrari",
+        Price: 50000000,
+        made: 2020,
+        color: "Red"
+    }
+}
+
+console.log(student);
